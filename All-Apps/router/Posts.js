df90199faf39df9aa771c0629911649994358270
Mhmd8998 <alnasrmhmd9988@gmail.com>
2024-12-17 15:48:48 +0300

    Update Posts.js

--- All-Apps/router/Posts.js
+++ All-Apps/router/Posts.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const controller = require("../controlaer/Posts"); // تأكد من أن المسار صحيح
 
 router.post("/", controller.insertPost);
-
+router.get("/", controller.getAllPosts);
 module.exports = router;
