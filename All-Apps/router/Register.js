e0c710eb0ac8e4b31a9aef18f95c74ca48a6205b
Mhmd8998 <alnasrmhmd9988@gmail.com>
2024-12-18 22:57:20 +0300

    Update Register.js

--- All-Apps/router/Register.js
+++ All-Apps/router/Register.js
@@ -3,5 +3,6 @@ const router = app.Router();
 const controller = require("../controlaer/Register");
 
 router.post("/",controller.insertUser);
+router.post("/login",controller.login);
 
 module.exports = router;
