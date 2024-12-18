788f77ece97c43fc1bea622376478ffd8b0308d4
Mhmd8998 <alnasrmhmd9988@gmail.com>
2024-12-18 10:28:12 +0300

    Update Users.js

--- All-Apps/router/Users.js
+++ All-Apps/router/Users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const usercontroller = require('../controlaer/Users');
 
-router.post('/',usercontroller.insertUser);
 router.get('/',usercontroller.getAllUsers);
 router.put('/:name',usercontroller.updateUser);
 router.delete('/:name',usercontroller.deleteUser);
