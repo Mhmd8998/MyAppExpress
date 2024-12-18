c495fd4d7f3ef62a5eba83fa6531cb288422de8c
Mhmd8998 <alnasrmhmd9988@gmail.com>
2024-12-18 10:37:50 +0300

    Update Register.js

--- All-Apps/controlaer/Register.js
+++ All-Apps/controlaer/Register.js
@@ -1,1 +1,29 @@
 const jwt = require("jsonwebtoken");
+const UserModel = require("../models/Users");
+const bcrypt = require("bcrypt");
+
+
+module.exports= {
+  insertUser: async (req, res) => {
+        try {
+            const username = req.body.name;
+            const password = req.body.password;
+            const email = req.body.email;
+            const chickUser = UserModel.findOne({username})
+            if(chickUser){
+              return res.json({message:"Already exists "})
+            }
+	          const hashPass = bcrypt.hashSync(password,10);
+
+            const createuser = new UserModel({
+                name: username,
+                password: hashPass,
+                email: email
+            });
+            await createuser.save();
+            return res.json(createuser);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    },
+}
